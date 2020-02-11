6e534b4edad5a47c02fa0b14064c7d77ca29860b
Ciervos <55422681+Ciervos@users.noreply.github.com>
2020-02-11 14:31:32 -0300

    cambio por event.target

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Listadeproductos from './components/listadeproductos/listadeproductos';
 
 function App() {
+
+
   return (
    <Listadeproductos></Listadeproductos>
   );
--- src/components/listadeproductos/listadeproductos.js
+++ src/components/listadeproductos/listadeproductos.js
@@ -10,28 +10,53 @@ const Listadeproductos = props =>{
         precio: "40.00"
     }]);
     
+    const [id, setId] = useState();
+    const [name, setName] = useState();
+    const [precio, setPrecio] = useState();
+
+    const getId = event =>{
+        setId(event.target.value);
+    }
+    const getName = event =>{
+        setName(event.target.value);
+    }
+
+    const getPrecio = event =>{
+        setPrecio(event.target.value);
+    }
+
     const agregarLista = () =>{
         
-    let inputcont = document.querySelector("#inputprincipal").value;    
+    /*let inputcont = document.querySelector("#inputprincipal").value;    
     inputcont = inputcont.split(",");
 
     const nuevoelemento = {
              id: inputcont[0],
              name: inputcont[1],
              precio: inputcont[2],
-         }
+         }*/
+     
+     const nuevoElemento = {
+         id,
+         name,
+         precio,
+     }    
 
-        setMaindata([...maindata, nuevoelemento]);
+        setMaindata([...maindata, nuevoElemento]);
   
     }
 
     const eliminarLista = () =>{
         console.log("me ejecuto -eliminarLista");
-        let inputcont = document.querySelector("#inputprincipal").value;    
+        /*let inputcont = document.querySelector("#inputprincipal").value;    
         inputcont = inputcont.split(",");
-        let targetid =  inputcont[0];
+        let targetid =  inputcont[0];*/
+        let targetid = id;
+        let targetname = name;
+        let targetprecio = precio;
         setMaindata(maindata => maindata.filter(item => item.id !== targetid));
-    
+        /*setMaindata(maindata => maindata.filter(item => item.name !== targetname));
+        setMaindata(maindata => maindata.filter(item => item.precio !== targetprecio));*/  
     }
 
 
@@ -41,7 +66,9 @@ const Listadeproductos = props =>{
     
         return(
     <div className="lista-contenedorgeneral">
-        <div><input id="inputprincipal" type="text" placeholder="ID,Nombre,Precio."></input>
+        <div><div className="input-contenedor"><input id="inputprincipal" onChange={getId} type="text" placeholder="ID"></input>
+        <input id="inputprincipal" type="text" onChange={getName} placeholder="Nombre"></input>
+        <input id="inputprincipal" type="text" onChange={getPrecio} placeholder="Precio"></input></div>
         <button type="submit" onClick={agregarLista}>Agregar</button>
         <button type="submit">Modificar</button>
         <button type="submit" onClick={eliminarLista}>Eliminar</button></div>
